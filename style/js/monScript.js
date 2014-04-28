870237bc68f948a760f75d754c30d7301957d8f4
Camille Dewavrin <camilledewavrin@hotmail.fr>
2014-04-28 15:28:16 +0200

    site v2

---
+++ style/js/monScript.js
@@ -0,0 +1,18 @@
+$(function() {
+	/* Third demo */
+	$('#demo3_thumbs').desoSlide({
+		main: {
+			container: '#demo3_main_image',
+			cssClass: 'img-responsive'
+		},
+		effect: 'flip',
+		overlay: 'hover',
+		caption: true,
+		controls: {
+			enable: false,
+			keys: false
+		}
+	});
+
+
+});
